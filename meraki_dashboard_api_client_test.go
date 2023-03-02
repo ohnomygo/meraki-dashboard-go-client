1b29827f69b0b96ea8987b3863c36f6e5024b49a
Oh No My Go <121621938+ohnomygo@users.noreply.github.com>
2023-03-02 11:00:14 +0100

    adding unit test for client

---
+++ meraki_dashboard_api_client_test.go
@@ -0,0 +1,23 @@
+package client
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/dashboard-api-go-swag/client"
+	"github.com/dashboard-api-go-swag/client/organizations"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+)
+
+/*
+This is a test using the meraki client.
+*/
+func TestClient(t *testing.T) {
+	params := organizations.NewGetOrganizationsParams()
+	client := client.New(httptransport.New(client.DefaultHost, client.DefaultBasePath, nil), strfmt.Default)
+	apiKeyHeaderAuth := httptransport.APIKeyAuth("X-Cisco-Meraki-API-Key", "header", os.Getenv("MERAKI_API_KEY"))
+	resp, _ := client.Organizations.GetOrganizations(params, apiKeyHeaderAuth)
+	fmt.Println(resp.Payload[0].Name)
+}
